2c31b5c7265c2d39d3e0f4325a94020fa73bcf0d
SkyXcripto {dev} <producer@skynance.net>
2021-11-28 17:42:56 -0300

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
-
 import { ToastContainer } from 'react-toastify'
 
 import 'react-toastify/dist/ReactToastify.css'
 import '@styles/globals.css'
 
-const Noop = ({children}) => <>{children}</>
+const Noop = ({ children }) => <>{children}</>
 
 function MyApp({ Component, pageProps }) {
-
   const Layout = Component.Layout ?? Noop
 
   return (
